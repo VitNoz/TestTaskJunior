b9af289215f174091ed4cbdfff5ae28d53c086d4
VitNoz <vitnoz@icloud.com>
2022-10-08 17:12:18 +0300

    new test commit

--- TestTask/View/View/PostDetails.swift
+++ TestTask/View/View/PostDetails.swift
@@ -153,15 +153,4 @@ class PostDetailsViewController: UIViewController {
             //dateLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10)
         ])
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
